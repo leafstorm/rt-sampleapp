aaabee95b6d70efee79fca3defc485a6a47924ba
Matthew Frazier <leafstormrush@gmail.com>
2011-04-24 10:06:03 -0400

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,6 @@
+# Ruby on Rails Tutorial: Sample Application
+
+This is a sample social networking application (I think) for
+[*Ruby on Rails 3 Tutorial*](http://www.railstutorial.org/) by
+[Michael Hartl](http://www.michaelhartl.com/). This one will probably stick
+around longer than first_app.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '20bbc665b815f6ff3877e8380479c68eb03cd0f2571b5f9c5593d14482b2e6fa6061ee0f1aa5aced94b4c70c81954a1feeb54d7ba0b3cae25338f052c9479eed'
